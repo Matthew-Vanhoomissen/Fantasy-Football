3629f5c9651aa2d6ac064ccce9f13bb71f7dac5a
Matthew-Vanhoomissen <ma.vanhoom1@gmail.com>
2025-10-13 15:17:23 -0700

    fixed passing td calc

--- scripts/player_stats_data.py
+++ scripts/player_stats_data.py
@@ -1,13 +1,15 @@
 import pandas as pd
 
-pdp = pd.read_csv("../data/J.Allen_data_2025.csv", low_memory=False)
-print("Josh Allen stats")
+pdp = pd.read_csv("../data/player1_data_2025.csv", low_memory=False)
+print("Player stats")
 
-total_P_Yards = pdp['passing_yards'].sum()
+player_name = 'T.Kraft'
+
+total_P_Yards = pdp[pdp['passer_player_name'] == player_name]['passing_yards'].sum()
 print("Total passing yards: ")
 print(total_P_Yards)
 
-total_R_Yards = pdp[pdp['rusher_player_name'] == 'J.Allen']['rushing_yards'].sum()
+total_R_Yards = pdp[pdp['rusher_player_name'] == player_name]['rushing_yards'].sum()
 print(f"Total rushing yards: {total_R_Yards}")
 
 games_played = pdp['week'].nunique()
@@ -18,23 +20,28 @@ average_R_Yards = total_R_Yards/games_played
 print(f"Average yards per game: {average_P_Yards}")
 print(f"Average rushing yards: {average_R_Yards}")
 
-receiving_yards = pdp[pdp['receiver_player_name'] == 'J.Allen']['receiving_yards'].sum()
+receptions = len(pdp[(pdp['receiver_player_name'] == player_name) & (pdp['receiving_yards'] > 0)])
+print(f"Total receptions: {receptions}")
+
+receiving_yards = pdp[pdp['receiver_player_name'] == player_name]['receiving_yards'].sum()
 print(f"Total receiving yards: {receiving_yards}")
 
 interceptions = pdp['interception'].sum()
 print(f"Total interceptions: {interceptions}")
 
-fumbles_lost = pdp[pdp['fumbled_1_player_name'] == 'J.Allen']['fumble_lost'].sum()
+fumbles_lost = pdp[pdp['fumbled_1_player_name'] == player_name]['fumble_lost'].sum()
 print(f"Fumbles lost: {fumbles_lost}")
 
-pTd = pdp['pass_touchdown'].sum()
+pTd = pdp[pdp['passer_player_name'] == player_name]['pass_touchdown'].sum()
 print(pTd)
-rtd = pdp[pdp['rusher_player_name'] == 'J.Allen']['rush_touchdown'].sum()
+rtd = pdp[pdp['rusher_player_name'] == player_name]['rush_touchdown'].sum()
 print(rtd)
+recTd = pdp[pdp['receiver_player_name'] == player_name]['pass_touchdown'].sum()
+print(recTd)
 
 print(f"Total tds: {pTd + rtd}")
 
-total_F_Points = (total_P_Yards * .04) + (total_R_Yards * .1) + (receiving_yards * .1) - (interceptions * 2) + (pTd * 4) + (rtd * 6) - (fumbles_lost * 2)
+total_F_Points = (total_P_Yards * .04) + (total_R_Yards * .1) + (receiving_yards * .1) - (interceptions * 2) + (pTd * 4) + (rtd * 6) + (recTd * 6) - (fumbles_lost * 2) + (receptions)
 average_F_Points = total_F_Points / games_played
 print(f"Total fantasy points: {total_F_Points}, Average points per game: {average_F_Points}")
 
@@ -52,15 +59,15 @@ for week_num in weeks:
     week_data = pdp[pdp['week'] == week_num]
     
     total_P_Yards = week_data['passing_yards'].sum()
-    total_R_Yards = week_data[week_data['rusher_player_name'] == 'J.Allen']['rushing_yards'].sum()
-    receiving_yards = week_data[week_data['receiver_player_name'] == 'J.Allen']['receiving_yards'].sum()
+    total_R_Yards = week_data[week_data['rusher_player_name'] == player_name]['rushing_yards'].sum()
+    receiving_yards = week_data[week_data['receiver_player_name'] == player_name]['receiving_yards'].sum()
     interceptions = week_data['interception'].sum()
-    fumbles_lost = week_data[week_data['fumbled_1_player_name'] == 'J.Allen']['fumble_lost'].sum()
-    pTd = week_data['pass_touchdown'].sum()
-    rtd = week_data[week_data['rusher_player_name'] == 'J.Allen']['rush_touchdown'].sum()
-    
+    fumbles_lost = week_data[week_data['fumbled_1_player_name'] == player_name]['fumble_lost'].sum()
+    pTd = week_data[week_data['passer_player_name'] == player_name]['pass_touchdown'].sum()
+    rtd = week_data[week_data['rusher_player_name'] == player_name]['rush_touchdown'].sum()
+    recTd = week_data[week_data['receiver_player_name'] == player_name]['pass_touchdown'].sum()
     # Fantasty points from that week
-    total_F_Points_W = (total_P_Yards * .04) + (total_R_Yards * .1) + (receiving_yards * .1) - (interceptions * 2) + (pTd * 4) + (rtd * 6) - (fumbles_lost * 2)
+    total_F_Points_W = (total_P_Yards * .04) + (total_R_Yards * .1) + (receiving_yards * .1) - (interceptions * 2) + (pTd * 4) + (rtd * 6) + (recTd * 6) - (fumbles_lost * 2)
     
     print(f"Week {week_num}: {total_F_Points_W} fantasy points")
 
--- scripts/testing_player_data.py
+++ scripts/testing_player_data.py
@@ -5,12 +5,16 @@ import pandas as pd
 pbp = pd.read_csv("../data/play_by_play_2025.csv", low_memory=False)
 
 # Search for a specific player
-player_name = "J.Allen"
+player_name = "T.Kraft"
 
 # Filter the dataframe
-player_data = pbp[pbp['name'] == player_name]
+player_data = pbp[
+    (pbp['passer_player_name'] == player_name) |
+    (pbp['rusher_player_name'] == player_name) |
+    (pbp['receiver_player_name'] == player_name)
+]
 
 # Save to new csv
-player_output_path = f"../data/{player_name.replace(' ', '_')}_data_{2025}.csv"
+player_output_path = f"../data/player1_data_{2025}.csv"
 player_data.to_csv(player_output_path, index=False)
 print(f"Data saved to {player_output_path}")
